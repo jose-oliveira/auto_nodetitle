c0e5d21f66a889831813e711d280c4088cade7ae
blesss <bless@internetdevels.com>
2014-07-14 11:31:13 +0300

    #2141845 patch by blesss: port into drupal 8

--- auto_nodetitle.js
+++ auto_nodetitle.js
@@ -2,11 +2,9 @@
 
 Drupal.behaviors.auto_nodetitleFieldsetSummaries = {
   attach: function (context) {
-    $('fieldset#edit-auto-nodetitle', context).drupalSetSummary(function (context) {
-
+    $('details#edit-auto-nodetitle', context).drupalSetSummary(function (context) {
       // Retrieve the value of the selected radio button
-      var ant = $("input[@name=#edit-auto-nodetitle-ant]:checked").val();
-
+      var ant = $(".form-item-auto-nodetitle-status input:checked").val();
       if (ant==0) {
         return Drupal.t('Disabled')
       }
